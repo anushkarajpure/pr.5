c67076ab432b814c43cb0bd20d88cf3a0598dc2c
anushkarajpure <anushkarajpure802@gmail.com>
2024-11-25 18:19:49 +0530

    Add files via upload

---
+++ q.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int i,n;
+	printf("enter size of array:");
+	scanf("%d",&n);
+	
+	printf("enter elements array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	printf("\nallnegative elements array:");
+	for(i=0;i<n;i++)
+	
+}
